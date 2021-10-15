7a8fcd0bd02b464e181bcb28f091f4a1202207d2
Sergey-Draft <drfatkrot1987@gmail.com>
2021-10-15 20:41:51 +0300

    hashRuoter for Pages

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import MainPage from './components/MainPage/MainPage';
 import Contacts from './components/Contacts/Contacts';
 import Introduce from './components/Introduce/Introduce';
 import MyMethod from './components/MyMethod/MyMethod';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Route } from 'react-router';
 import { Redirect } from 'react-router';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
 {    <link href="https://fonts.googleapis.com/css2?family=Cardo:ital@1&family=Open+Sans:wght@400;700&family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet"></link>}
 {<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet"></link>}
 {<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>}
@@ -26,7 +26,7 @@ function App() {
       <Route path='/mymethod' component={MyMethod}></Route>
       
     </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
